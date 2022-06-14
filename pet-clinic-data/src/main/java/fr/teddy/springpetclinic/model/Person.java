a42e6c3f83b25769c61cfa4f6d0f356ece348584
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-06-14 16:38:06 +0200

    Adding base entity. Closes #10.

--- pet-clinic-data/src/main/java/fr/teddy/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/fr/teddy/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package fr.teddy.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
 	private String firstName;
 	private String lastName;
--- pet-clinic-data/src/main/java/fr/teddy/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/fr/teddy/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package fr.teddy.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
 	private String name;
 
