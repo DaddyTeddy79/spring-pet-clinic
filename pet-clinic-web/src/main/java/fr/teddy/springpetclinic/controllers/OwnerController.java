27e360158d56bc519cb7210dc16436851b0ee242
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-06-16 22:50:34 +0200

    Adding Owner List Page and Controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/fr/teddy/springpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package fr.teddy.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owner")
+public class OwnerController {
+
+	@RequestMapping({ "", "/", "/index", "/index.html" })
+	public String listOwners() {
+		return "owner/index";
+	}
+}
