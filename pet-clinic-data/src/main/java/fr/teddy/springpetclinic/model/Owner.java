c25078d2502d5838c2ed2b921847a8f29602b68d
DaddyTeddy79 <tcoudiere@yahoo.fr>
2022-06-26 14:24:36 +0200

    Adding contact info to Onwner. Closes #27

--- pet-clinic-data/src/main/java/fr/teddy/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/fr/teddy/springpetclinic/model/Owner.java
@@ -4,7 +4,43 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+	private String address;
+	private String city;
+	private String zipCode;
+	private String telephone;
 	private Set<Pet> pets;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;
